369f0fdcb99b020667a2d9dc5e32f80958a44515
Steven Vance <steve@stevevance.net>
2014-11-02 22:45:20 -0600

    use full list of PINs now

--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -9,7 +9,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     name = "propertyinfo"
     allowed_domains = ["www.cookcountypropertyinfo.com"]
     start_urls = (
-        'http://www.chicagocityscape.com/propertytaxes/300_n_state_cleaned_2x.txt',
+        'http://www.chicagocityscape.com/propertytaxes/300_n_state_cleaned.txt',
     )
 
     def parse_row(self, response, row):
