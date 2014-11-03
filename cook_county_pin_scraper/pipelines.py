a4a0f079e8318d7d9234736166fc72b81299bad2
Steven Vance <steve@stevevance.net>
2014-11-02 22:44:43 -0600

    Revert "change pipeline"
    This reverts commit 52a71c8795e0836927f26fef051ce53d5f539d90.

--- cook_county_pin_scraper/pipelines.py
+++ cook_county_pin_scraper/pipelines.py
@@ -1,11 +1,11 @@
-import json
+# -*- coding: utf-8 -*-
 
-class JsonWriterPipeline(object):
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-    def __init__(self):
-        self.file = open('items.jl', 'wb')
 
+class CookCountyPinScraperPipeline(object):
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
         return item
