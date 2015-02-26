87b1f3aeb2f5bc5c4afedb1bb28bf6526ff35cf4
Steven Vance <steve@stevevance.net>
2015-02-26 01:39:31 -0600

    changing source to 1st of 4

--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -9,7 +9,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     name = "propertyinfo"
     allowed_domains = ["www.cookcountypropertyinfo.com"]
     start_urls = (
-        'http://www.chicagocityscape.com/propertytaxes/1000.txt',
+        'http://www.chicagocityscape.com/propertytaxes/500000.txt',
     )
 
     def parse_row(self, response, row):
