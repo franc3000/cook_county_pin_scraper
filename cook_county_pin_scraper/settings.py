90cc4477764513eb721844f5350841f38820ebbd
Steven Vance <steve@stevevance.net>
2015-03-03 00:02:28 -0600

    changed settings to reduce concurrent requests for memory improvement

--- cook_county_pin_scraper/settings.py
+++ cook_county_pin_scraper/settings.py
@@ -12,7 +12,10 @@ BOT_NAME = 'cook_county_pins'
 
 SPIDER_MODULES = ['cook_county_pin_scraper.spiders']
 NEWSPIDER_MODULE = 'cook_county_pin_scraper.spiders'
-DOWNLOAD_DELAY = 0.04
+DOWNLOAD_DELAY = 0.03
+CONCURRENT_REQUESTS = 10
+MEMDEBUG_ENABLED = True
+MEMDEBUG_NOTIFY = ['jamesbondsv@gmail.com']
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cook_county_pin_scraper (+http://www.yourdomain.com)'
