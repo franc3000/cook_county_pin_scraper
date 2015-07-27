6c101090c7ceef16117b035293350a972243999b
Steven Vance <steve@stevevance.net>
2015-07-27 18:32:58 -0500

    change performance settings

--- cook_county_pin_scraper/settings.py
+++ cook_county_pin_scraper/settings.py
@@ -12,10 +12,10 @@ BOT_NAME = 'cook_county_pins'
 
 SPIDER_MODULES = ['cook_county_pin_scraper.spiders']
 NEWSPIDER_MODULE = 'cook_county_pin_scraper.spiders'
-DOWNLOAD_DELAY = 0.03
-CONCURRENT_REQUESTS = 13
+DOWNLOAD_DELAY = 0.035
+CONCURRENT_REQUESTS = 12
 MEMDEBUG_ENABLED = True
-MEMDEBUG_NOTIFY = ['jamesbondsv@gmail.com']
+#MEMDEBUG_NOTIFY = ['jamesbondsv@gmail.com']
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cook_county_pin_scraper (+http://www.yourdomain.com)'
