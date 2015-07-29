a3b6f0180edea11112c0440ed49fbdd86bdd5fb1
Steven Vance <steve@stevevance.net>
2015-07-29 07:43:58 -0500

    new source: 900,000

--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -9,7 +9,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     name = "propertyinfo"
     allowed_domains = ["cookcountypropertyinfo.com"]
     start_urls = [
-    	"http://www.chicagocityscape.com/propertytaxes/pins_lists/800001-900000.txt"
+    	"http://www.chicagocityscape.com/propertytaxes/pins_lists/900001-1000000.txt"
     ]
 
     def parse_row(self, response, row):
