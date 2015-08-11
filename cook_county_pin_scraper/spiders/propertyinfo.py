b5ee4a6a6a4ff24d8ac1090af60d0d847bdf30a1
Steven Vance <steve@stevevance.net>
2015-08-11 09:25:37 -0500

    new source: found_in_permits.txt

--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -9,7 +9,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     name = "propertyinfo"
     allowed_domains = ["cookcountypropertyinfo.com"]
     start_urls = [
-    	"http://www.chicagocityscape.com/propertytaxes/pins_lists/found_in_parcel_2013.txt"
+    	"http://www.chicagocityscape.com/propertytaxes/pins_lists/found_in_permits.txt"
     ]
 
     def parse_row(self, response, row):
