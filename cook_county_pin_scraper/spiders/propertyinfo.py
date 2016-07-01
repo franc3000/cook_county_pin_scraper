961ce2f1fee856d8a8aef25415aadf8ca52d6c29
Steven Vance <steve@stevevance.net>
2016-07-01 22:20:24 +0200

    batch part 2

--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -9,7 +9,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     name = "propertyinfo"
     allowed_domains = ["cookcountypropertyinfo.com"]
     start_urls = [
-    	"http://www.chicagocityscape.com/propertytaxes/pins_lists/0-250000.csv"
+    	"http://www.chicagocityscape.com/propertytaxes/pins_lists/250001-500000.csv"
     ]
     state = OrderedDict()
 
