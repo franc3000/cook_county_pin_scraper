17c59b747c7874fff7f2e869db03995d6f333791
Steven Vance <steve@stevevance.net>
2016-09-06 09:04:01 -0500

    change source URL

--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -11,7 +11,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     name = "propertyinfo"
     allowed_domains = ["cookcountypropertyinfo.com"]
     start_urls = [
-    	"file:///cook_county_pin_scraper/cook_county_pin_scraper/lists/1882964.csv"
+    	"file:///cook_county_pin_scraper/lists/1882964.csv"
     ]
     state = OrderedDict()
 
