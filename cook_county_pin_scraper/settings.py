d82857278e216e83664a0c5c58964f540c6201ac
Steven Vance <steve@stevevance.net>
2016-09-11 15:31:19 -0500

    more testing

--- cook_county_pin_scraper/settings.py
+++ cook_county_pin_scraper/settings.py
@@ -12,7 +12,7 @@ BOT_NAME = 'cook_county_pins'
 
 SPIDER_MODULES = ['cook_county_pin_scraper.spiders']
 NEWSPIDER_MODULE = 'cook_county_pin_scraper.spiders'
-DOWNLOAD_DELAY = 0.009
+DOWNLOAD_DELAY = 0.1
 CONCURRENT_REQUESTS = 20
 MEMDEBUG_ENABLED = True
 COOKIES_DEBUG = False
