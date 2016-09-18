8983751b3f7d0e4b9e91aad65fb9dd385bc1d75e
Steven Vance <steve@stevevance.net>
2016-09-17 23:20:27 -0500

    new source; also going 1 at a time so I can troubleshoot errors

--- cook_county_pin_scraper/settings.py
+++ cook_county_pin_scraper/settings.py
@@ -12,8 +12,8 @@ BOT_NAME = 'cook_county_pins'
 
 SPIDER_MODULES = ['cook_county_pin_scraper.spiders']
 NEWSPIDER_MODULE = 'cook_county_pin_scraper.spiders'
-DOWNLOAD_DELAY = 0.008
-CONCURRENT_REQUESTS = 30
+DOWNLOAD_DELAY = 0.20
+CONCURRENT_REQUESTS = 1
 MEMDEBUG_ENABLED = True
 COOKIES_DEBUG = False
 DOWNLOAD_TIMEOUT = 1000
--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -11,7 +11,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     name = "propertyinfo"
     allowed_domains = ["cookcountypropertyinfo.com"]
     start_urls = [
-    	"file:///cook_county_pin_scraper/lists/49689.txt"
+    	"file:///cook_county_pin_scraper/lists/35504.txt"
     ]
     state = OrderedDict()
 
