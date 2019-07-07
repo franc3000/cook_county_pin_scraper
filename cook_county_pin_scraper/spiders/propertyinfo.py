278375f5573f72f907875c024a311845f649cefb
Steven Vance <steve@stevevance.net>
2019-07-06 22:54:28 -0500

    change to batch 5

--- cook_county_pin_scraper/spiders/propertyinfo.py
+++ cook_county_pin_scraper/spiders/propertyinfo.py
@@ -14,7 +14,7 @@ class PropertyinfoSpider(CSVFeedSpider):
     start_urls = [
         #"file:///Users/stevevance/Sites/cook_county_pin_scraper/lists/batch1.csv"
         #"file:///Users/stevevance/Sites/cook_county_pin_scraper/lists/sample.csv"
-        "http://chicagocityscape.com/scrapy/batch4.csv"
+        "http://chicagocityscape.com/scrapy/batch5.csv"
     ]
     state = OrderedDict()
 
